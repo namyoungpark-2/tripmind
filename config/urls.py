819d22674d1dcf12682860acbba617eaa1ef5e3d
namyoungpark <namyoungpark@odkmedia.net>
2025-05-22 22:02:22 +0900

    apply lint in config

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/tripmind/', include('tripmind.api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/tripmind/", include("tripmind.api.urls")),
 ]
